e21fb618cd0eb0af124944b1b4f92bbeaad4174b
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-02-25 10:34:39 +0900

    first commit

---
+++ android/app/src/main/java/com/gcc/miti/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gcc.miti;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,36 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const commonPlugins = [
+  "@capacitor/app",
+  "@capacitor/browser",
+  "@capacitor/keyboard",
+];
+const androidPlugins: string[] = [];
+const iosPlugins = ["capacitor-plugin-safe-area"];
+
+//* ********************************************************************************************* */
+
+const url = "https://dev-miti.dockerfile.site";
+
+const server: CapacitorConfig["server"] = {
+  hostname: "localhost",
+  androidScheme: "http",
+  iosScheme: "http",
+  url,
+  cleartext: true,
+};
+
+const config: CapacitorConfig = {
+  appId: "com.gcc.miti",
+  appName: "miti",
+  bundledWebRuntime: false,
+  android: {
+    includePlugins: [...commonPlugins, ...androidPlugins],
+  },
+  ios: {
+    includePlugins: [...commonPlugins, ...iosPlugins],
+  },
+  server,
+};
+
+export default config;
